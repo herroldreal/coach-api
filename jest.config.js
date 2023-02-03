f65ce8d0c6b5a42fe35eef7b56ce2c64d705ba11
Herrold Real <herrold.real@go1.com>
2023-02-03 16:55:08 -0600

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>'],
+  setupFiles: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@App/(.*)': '<rootDir>/src/$1',
+    '^@Test/(.*)': '<rootDir>/tests/$1',
+  },
+  setupFilesAfterEnv: ['./src/test/setup.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**', '!**/tests/**'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+};
---
+++ tests/face-recognition/index.test.ts
@@ -0,0 +1,5 @@
+describe('Face Recognition - Test Suite', () => {
+  it('test', () => {
+    expect(true).toBeTruthy();
+  });
+});
